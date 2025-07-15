154cfc9acbfeb8716d8a68e2ede7a5dd0f1550dd
myfavoritecolorisyou <eunseokleedev@gmail.com>
2025-07-15 21:54:33 +0900

    initial commit

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber != 0;
+        }
+        public int Id { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+    }
+}
